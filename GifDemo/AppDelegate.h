e68422ca1f84a21b80287f016a8abcc15bffcc93
chendan <chendan@macbook-pro-2.local>
2017-12-11 15:36:33 +0800

    Initial Commit

---
+++ GifDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GifDemo
+//
+//  Created by chendan on 2017/12/11.
+//  Copyright © 2017年 chendan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GifDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GifDemo
+//
+//  Created by chendan on 2017/12/11.
+//  Copyright © 2017年 chendan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
